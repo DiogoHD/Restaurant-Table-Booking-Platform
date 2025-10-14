020445a1d2254a76a526cf727ca2b26742d6dafd
Diogo Neves <nevesdiogo2006@gmail.com>
2025-10-14 23:25:24 +0100

    Phone number and number of guest per table validation

--- reservations/models.py
+++ reservations/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Table(models.Model):
@@ -22,3 +23,11 @@ class Reservation(models.Model):
     
     def __str__(self):
         return f"{self.first_name} {self.last_name} - Table {self.table.number} at {self.date}"
+    
+    # Clean allows form validation
+    def clean(self):
+        if self.table and self.guests > self.table.seats:
+            raise ValidationError(f"The table {self.table.number} only has {self.table.seats} seats and you have {self.guests} guests.")
+        
+        if len(str(self.phone_number)) != 9:
+            raise ValidationError(f"Your phone number must have 9 digits")
