e2f2cb783dd068550bcf4916006a8734946f9982
Diogo Neves <nevesdiogo2006@gmail.com>
2025-10-15 00:17:42 +0100

    Better Admin Visualization

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Table, Reservation
+
+from .models import Reservation, Table
+
 
 # Register your models here.
+class TableAdmin(admin.ModelAdmin):
+    list_display = ("table", "date", "first_name", "last_name", "guests", "phone_number", "notes")
+
 admin.site.register(Table)
-admin.site.register(Reservation)
+admin.site.register(Reservation, TableAdmin)
