e02cdb4cab6dd2fcfe87893dfe81ea73c97f09e6
Diogo Neves <nevesdiogo2006@gmail.com>
2025-10-14 21:56:12 +0100

    Table and Reservarion Models

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Table(models.Model):
+    number = models.PositiveSmallIntegerField(unique=True)
+    seats = models.PositiveSmallIntegerField()
+    
+    def __str__(self):
+        return f"Table {self.number} ({self.seats} seats)"
+
+
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.PositiveIntegerField()
+    date = models.DateTimeField()
+    guests = models.PositiveSmallIntegerField()
+    # Foreign Key allows for multiple reservations in the same table
+    # on_delete=models.CASCADE means that if the table is deleted, all reservations in that table are deleted too
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    notes = models.TextField(blank=True)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - Table {self.table.number} at {self.date}"
