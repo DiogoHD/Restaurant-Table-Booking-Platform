cc67033b72e6f3ae7052568fc4f31fe16d01907d
Diogo Neves <nevesdiogo2006@gmail.com>
2025-10-14 23:03:43 +0100

    First Template

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Table
+
 
 # Create your views here.
+def tables(request):
+    available_tables = Table.objects.all()
+    template = loader.get_template("main.html")
+    context = {"available_tables": available_tables}
+    return HttpResponse(template.render(context, request))
