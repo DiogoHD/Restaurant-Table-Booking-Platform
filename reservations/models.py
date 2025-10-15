bc1f693ef4742044e305d9f484372f7157a5341c
Diogo Neves <nevesdiogo2006@gmail.com>
2025-10-15 11:57:35 +0100

    Automatically assigns a table if the user doesn't choose one

--- reservations/models.py
+++ reservations/models.py
@@ -21,11 +21,28 @@ class Reservation(models.Model):
     guests = models.PositiveSmallIntegerField()
     # Foreign Key allows for multiple reservations in the same table
     # on_delete=models.CASCADE means that if the table is deleted, all reservations in that table are deleted too
-    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE, blank=True, null=True)
     notes = models.TextField(blank=True)
     
     def __str__(self):
         return f"{self.first_name} {self.last_name} - Table {self.table.number} at {self.date}"
+    
+    def is_available(self, table: Table) -> bool:
+        # Filters all the objects for the table
+        start_time = self.date
+        end_time = self.date + RESERVATION_DURATION
+        
+        # Loops through all reservations in that table
+        # exclude(pk=self.pk) ignores self object whn editing the data base
+        for r in Reservation.objects.filter(table=table).exclude(pk=self.pk):     
+            r_start = r.date
+            r_end = r.date + RESERVATION_DURATION
+            
+            # If the new reservation starts before the existing reservation ends and the new reservation ends after the new reservation starts
+            if (start_time < r_end) and (end_time > r_start):
+                return False
+        
+        return True
     
     # Clean allows form validation
     def clean(self):
@@ -40,16 +57,15 @@ class Reservation(models.Model):
         
         # Checks if the table is available at that time (each reservation has a standard duration of 2 hours)
         if self.table:
-            # Filters all the objects for the table
-            start_time = self.date
-            end_time = self.date + RESERVATION_DURATION
+            if not self.is_available(self.table):
+                raise ValidationError(f"The table {self.table.number} is already booked for this time.")
+        
+        # If the user doesn't choose a table, a table is automatically assigned
+        else:
+            for t in Table.objects.all():
+                if self.guests <= t.seats and self.is_available(t):
+                    self.table = t
+                    break
             
-            # Loops through all reservations in that table
-            # exclude(pk=self.pk) ignores self object whn editing the data base
-            for r in Reservation.objects.filter(table=self.table).exclude(pk=self.pk):     
-                r_start = r.date
-                r_end = r.date + RESERVATION_DURATION
-                
-                # If the new reservation starts before the existing reservation ends and the new reservation ends after the new reservation starts
-                if (start_time < r_end) and (end_time > r_start):
-                    raise ValidationError(f"The table {self.table.number} is already booked for this time.")
+            if not self.table:
+                raise ValidationError(f"Sorry, the restaurant is fully booked at this time.")
