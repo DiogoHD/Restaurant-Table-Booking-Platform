3611553c6997d37f55b15c24428bd123309e63ea
Diogo Neves <nevesdiogo2006@gmail.com>
2025-10-17 16:47:40 +0100

    Type Annotations in views

--- reservations/views.py
+++ reservations/views.py
@@ -1,14 +1,15 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 from .models import Table
 
-def home(request):
+
+def home(request: HttpRequest) -> HttpResponse:
     return render(request, "home.html", {})
 
 # Create your views here.
-def tables(request):
+def tables(request: HttpRequest) -> HttpResponse:
     available_tables = Table.objects.all()
     template = loader.get_template("tables.html")
     context = {"available_tables": available_tables}
